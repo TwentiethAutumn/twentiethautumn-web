15016d4c541a2b6dfcd338325b2d550c48841520
TwentiethAutumn <firemonstr2002@mail.ru>
2023-10-17 17:11:49 +0300

    env options

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,12 @@ import {User} from "./user/entities/user.entity";
 import {Theme} from "./theme/entities/theme.entity";
 import {Comment} from "./comment/entities/comment.entity";
 import {Post} from "./post/entities/post.entity";
-import {UsersService} from "./user/user.service";
 import {UsersModule} from "./user/user.module";
-import {UsersController} from "./user/user.controller";
 import {ThemesModule} from "./theme/theme.module";
 import {PostsModule} from "./post/post.module";
 import {CommentsModule} from "./comment/comment.module";
 
-//const databaseUrl: string = process.env.DATABASE_URL; // PROD
-/*
-const databaseUrl: string = "PGPASSWORD=Eo0wCrJEJCBDld6gW7rTkEyvPKv7PWlj psql -h dpg-ck9fjlmgtj9c73ck1m10-a.frankfurt-postgres.render.com -U twentiethautumn twentiethautumn"; // DEBUG
+const databaseUrl: string = process.env.DATABASE_URL; // PROD
 const connectionOptions = PostgresConnectionStringParser.parse(databaseUrl);
 const typeOrmOptions: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -29,18 +25,6 @@ const typeOrmOptions: TypeOrmModuleOptions = {
   database: connectionOptions.database,
   entities: [User, Theme, Comment, Post],
   synchronize: false,
-}
- */
-
-const typeOrmOptions: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'dpg-ck9fjlmgtj9c73ck1m10-a.frankfurt-postgres.render.com',
-  port: 5432,
-  username: 'twentiethautumn',
-  password: 'Eo0wCrJEJCBDld6gW7rTkEyvPKv7PWlj',
-  database: 'twentiethautumn',
-  entities: [User, Theme, Comment, Post],
-  synchronize: true,
   ssl: true,
   extra: {
     rejectUnauthorized: false,
